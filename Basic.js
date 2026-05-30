6fd648f9b60b9137f97e8e38ef0f8a6e765267bf
aruninaws@gmail.com <arunelangovan@ArunElangovans-MacBook-Pro.local>
2026-05-30 14:35:04 +0530

    Day 1 - JavaScript basics

---
+++ Basic.js
@@ -0,0 +1,2 @@
+let myname = "Arun";
+console.log("Hello" + myname);
---
+++ Variables.js
@@ -0,0 +1,7 @@
+let firstName = "Arun";
+let secondName = "Elangovan";
+let age = "25";
+let city = "Bangalore";
+console.log( "My Name is" + " "+ firstName  + secondName);
+console.log("My age is" +" "+ age);
+console.log("I am living in"+ city);
